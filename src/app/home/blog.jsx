35b1267c85943caba56abf941f3881c03ddb26e3
mdsharia4613 <mdsharia4613@gmail.com>
2025-11-21 10:37:24 +0600

    blog img add

---
+++ src/app/home/blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import blogs from "@/data/blog.json";
+
+const blog = () => {
+    return (
+        <div>
+            J=
+        </div>
+    );
+};
+
+export default blog;
